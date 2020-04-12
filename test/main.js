c3c2abcc968b1a8bd6cf938e95cd23d99c58163c
konglingwen94 <794430774@qq.com>
2020-04-12 14:02:26 +0800

    Add

--- test/main.js
+++ test/main.js
@@ -1,36 +1,38 @@
 import Vue from 'vue'
 import MessageTree from '../lib/message-tree.umd.min.js'
+import dataList from './data.json'
 
 Vue.use(MessageTree)
+console.log(dataList)
 
 new Vue({
   el: '#app',
   created() {
-    fetch('http://localhost:7001/api/messages')
-      .then(response => response.json())
-      .then(data => {
-        this.dataList = data
-      })
+    // fetch('http://localhost:7001/api/messages')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.dataList = data
+    //   })
   },
   data: {
-    dataList: [],
+    dataList,
   },
-  computed: {
-    renderData() {
-      function format(list, originalList) {
-        list.forEach(item => {
-          item.children = originalList.filter(sub => sub.parentID === item.id)
-          format(item.children, originalList)
-        })
-        return list
-      }
-      return format(this.dataList.filter(item => !item.parentID), this.dataList)
-    }
-  },
+  // computed: {
+  //   renderData() {
+  //     function format(list, originalList) {
+  //       list.forEach(item => {
+  //         item.children = originalList.filter(sub => sub.parentID === item.id)
+  //         format(item.children, originalList)
+  //       })
+  //       return list
+  //     }
+  //     return format(this.dataList.filter(item => !item.parentID), this.dataList)
+  //   }
+  // },
   render(h) {
     return h('message-tree', {
       props: {
-        dataList: this.renderData,
+        dataList: this.dataList,
       },
     })
   },
